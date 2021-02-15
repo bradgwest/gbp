3e585eea97c7966f4aae421782d58000252fb19e
Brad West <brad.g.west@gmail.com>
2021-02-14 22:07:38 -0700

    Add some more stuff

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -3,23 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-var content = []byte(`
-<html>
-	<head>
-		<title>Chat</title>
-	</head>
-	<body>
-		Let's Chat!
-	</body>
-</html>
-`)
+// templateHandler represents a simple template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP Request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+
+	t.templ.Execute(w, nil)
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write(content)
-	})
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// start the webserver
 	if err := http.ListenAndServe(":8080", nil); err != nil {
